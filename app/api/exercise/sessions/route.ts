a55236b4dfd6906e4b54353392f5ff8f96ca5767
fn <affanhelmi111@gmail.com>
2026-01-12 22:06:26 +0700

    fix authjs using new version syntax

--- app/api/exercise/sessions/route.ts
+++ app/api/exercise/sessions/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/db";
+import {auth} from "@/auth";
 
-const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
     try {
-        const session = await getServerSession(authOptions);
+        const session = await auth();
 
         if (!session?.user?.email) {
             return NextResponse.json(
